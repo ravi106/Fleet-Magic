dd7b7e1105c0ce40e36b41cfa66790c5fe466180
Pradeep Meruva <pradeep.meruva@gmail.com>
2015-07-13 20:31:52 -0400

    comet

--- src/main/java/com/fleetmagic/config/AppConfig.java
+++ src/main/java/com/fleetmagic/config/AppConfig.java
@@ -1,23 +1,14 @@
 package com.fleetmagic.config;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
-
 @Configuration
-@ComponentScan(basePackages = "com.covisint")
+@ComponentScan(basePackages = "com.fleetmagic")
 @EnableAspectJAutoProxy
 class AppConfig {
     
-  /*  
-    @Bean
-    public HystrixCommandAspect hystrixAspect() {
-      return new HystrixCommandAspect();
-    }
-
-*/
+ 
 }
