1462530557429bdd5de64815aacb67f54317def1
Pradeep Meruva <pradeep.meruva@gmail.com>
2015-07-14 22:39:01 -0400

    Commit

--- src/main/java/com/fleetmagic/config/WebMvcConfig.java
+++ src/main/java/com/fleetmagic/config/WebMvcConfig.java
@@ -2,17 +2,15 @@ package com.fleetmagic.config;
 
 import java.util.Properties;
 
-import javax.sql.DataSource;
-
 import javax.annotation.Resource;
-
-import org.springframework.core.env.Environment;
+import javax.sql.DataSource;
 
 import org.hibernate.ejb.HibernatePersistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
