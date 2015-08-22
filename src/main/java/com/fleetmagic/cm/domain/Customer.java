21ca6df83c0ade1688809089a78872df5acfdeb9
Pradeep Meruva <pradeep.meruva@gmail.com>
2015-08-22 15:48:39 -0400

    Customer object created

--- src/main/java/com/fleetmagic/cm/domain/Customer.java
+++ src/main/java/com/fleetmagic/cm/domain/Customer.java
@@ -1,5 +1,31 @@
 package com.fleetmagic.cm.domain;
 
+import java.util.Date;
+
 public class Customer {
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private String homePhone;
+	private String mobile;
+	private String email;
+	private String address1;
+	private String address2;
+	private String city;
+	private String state;
+	private String zip;
+	private String ssn;
+	private Date dob;
+	private String driversLicence;
+	private String dlState;
+	private Date dlExperiryDate;
+	private String employer;
+	private String employerCity;
+	private String employerState;
+	
+	
+	
 
 }
