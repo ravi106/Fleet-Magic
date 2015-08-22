05a203d6dab8e936b425b4cb4aede9749b3f0ec7
Pradeep Meruva <pradeep.meruva@gmail.com>
2015-08-22 15:35:54 -0400

    sketal methods added to rental service

--- src/main/java/com/fleetmagic/rm/service/RentalService.java
+++ src/main/java/com/fleetmagic/rm/service/RentalService.java
@@ -1,5 +1,19 @@
 package com.fleetmagic.rm.service;
 
+import com.fleetmagic.rm.domain.Rental;
+
 public class RentalService {
+
+	public void createRental(Rental rental) {
+
+	}
+
+	public void updateRental(Rental rental) {
+
+	}
+
+	public void fetchRental(Rental rental) {
+
+	}
 
 }
