7b914daee5b7f6aee309b5501c2bc072c9db5a54
ravia <ravia@gamenous.com>
2016-08-30 18:56:11 +0530

    Client Integration initial commit

--- src/main/webapp/static/js/AgreementController.js
+++ src/main/webapp/static/js/AgreementController.js
@@ -6,4 +6,28 @@ angular.module('fleetMagic').controller('AgreementController',['$scope','fleetMa
 
     $scope.rentalResponse = fleetMagicService.getRentalAgreement();
     console.log($scope.rentalResponse);
+
+    $scope.printDiv = function () {
+        var printContents = document.getElementById("rentalAgreement").innerHTML;
+        var popupWin = window.open('', '_blank', 'width=300,height=300');
+        popupWin.document.open();
+        popupWin.document.write('<html><head><link rel="stylesheet" href="css/fleetMagic.css"></head><body onload="window.print()">' + printContents + '</body></html>');
+        popupWin.document.close();
+    };
+
+
+    $scope.saveAsPdf = function () {
+        html2canvas(document.getElementById('rentalAgreement'), {
+            onrendered: function (canvas) {
+                var data = canvas.toDataURL();
+                var docDefinition = {
+                    content: [{
+                        image: data
+                    }]
+                };
+                pdfMake.createPdf(docDefinition).download("Score_Details.pdf");
+            },
+            logging: true
+        });
+    }
 }]);
--- src/main/webapp/static/rental/RentalController.js
+++ src/main/webapp/static/rental/RentalController.js
@@ -4,6 +4,88 @@
 angular.module('fleetMagic').controller('RentalController', ['$scope', '$http', '$state', 'acuteSelectService', 'ClientConfig', 'fleetMagicService', function ($scope, $http, $state, acuteSelectService, ClientConfig, fleetMagicService) {
 
     $scope.fleetMagic = {};
+    $scope.formats = ['dd-MMMM-yyyy', 'MM/dd/yyyy', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[1];
+
+    $scope.dob = {
+        opened: false,
+        dateOptions: {
+            dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        }, inlineOptions: {
+            customClass: getDayClass,
+            minDate: new Date(),
+            showWeeks: true
+        }, toggleMin: function () {
+            $scope.dob.inlineOptions.minDate = $scope.dob.inlineOptions.minDate ? null : new Date();
+            $scope.dob.dateOptions.minDate = $scope.dob.inlineOptions.minDate;
+        },
+        open: function () {
+            $scope.dob.opened = true;
+        }
+    };
+    $scope.dob.toggleMin();
+    $scope.licenseExpiryDate = {
+        opened: false,
+        dateOptions: {
+            dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        }, inlineOptions: {
+            customClass: getDayClass,
+            minDate: new Date(),
+            showWeeks: true
+        }, toggleMin: function () {
+            $scope.licenseExpiryDate.inlineOptions.minDate = $scope.licenseExpiryDate.inlineOptions.minDate ? null : new Date();
+            $scope.licenseExpiryDate.dateOptions.minDate = $scope.licenseExpiryDate.inlineOptions.minDate;
+        },
+        open: function () {
+            $scope.licenseExpiryDate.opened = true;
+        }
+    };
+    $scope.licenseExpiryDate.toggleMin();
+
+    function disabled(data) {
+        var date = data.date,
+            mode = data.mode;
+        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    }
+
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 1);
+    $scope.events = [
+        {
+            date: tomorrow,
+            status: 'full'
+        },
+        {
+            date: afterTomorrow,
+            status: 'partially'
+        }
+    ];
+
+    function getDayClass(data) {
+        var date = data.date,
+            mode = data.mode;
+        if (mode === 'day') {
+            var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+            for (var i = 0; i < $scope.events.length; i++) {
+                var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+                if (dayToCheck === currentDay) {
+                    return $scope.events[i].status;
+                }
+            }
+        }
+        return '';
+    }
+
     $scope.initRentalController = function () {
         $http.get(ClientConfig.CLIENT_BASE_URL + "vehiclesByStatus/AVAILABLE").success(function (data) {
             $scope.fleetMagic.vehicles = data;
@@ -18,14 +100,6 @@ angular.module('fleetMagic').controller('RentalController', ['$scope', '$http', 
             }
         }).error(function (err) {
             console.log(err);
-        });
-    };
-
-     var mockData = function () {
-        //Temp for request payload
-        $http.get("mockData.json").success(function (res) {
-            console.log(res + "mock");
-            $scope.rental = res;
         });
     };
 
@@ -51,40 +125,47 @@ angular.module('fleetMagic').controller('RentalController', ['$scope', '$http', 
     $scope.payment = {};
     $scope.billing = {};
     $scope.customer = {};
+    $scope.rental = {};
     $scope.submitPaymentDetails = function () {
-        mockData();
+        $scope.rental.payment = {};
+        $scope.rental.payment.payStatus = "Approved";
+        $scope.rental.payment.description = "Paid";
+        $scope.rental.payment.payDate = 1443119400000;
+        $scope.rental.payment.amount = 76;
+        $scope.rental.payment.rentalPaymentId = 7;
+        $scope.rental.payment.paymentMethod = "DEBITCARD";
+        $scope.rental.payment.paidFor = null;
+        $scope.rental.payment.chequeNumber = null;
+        $scope.rental.payment.chequeIssuedBy = null;
+        $scope.rental.payment.cardInfo = {};
+        console.log($scope.payment);
 
-        // $scope.rental.payment = {};
-        // $scope.rental.payment.payStatus= "Approved";
-        // $scope.rental.payment.description="Paid";
-        // $scope.rental.payment.payDate=1443119400000;
-        // $scope.rental.payment.amount=76;
-        // $scope.rental.payment.rentalPaymentId=7;
-        // $scope.rental.payment.paymentMethod="Debit Card";
-        // $scope.rental.payment.paidFor=null;
-        // $scope.rental.payment.chequeNumber=null;
-        // $scope.rental.payment.chequeIssuedBy=null;
-        // $scope.rental.payment.cardInfo={};
-        // $scope.rental.payment.cardInfo.type="Debit";
-        // $scope.rental.payment.cardInfo.number="4356234589012341";
-        // $scope.rental.payment.cardInfo.expMonth=6;
-        // $scope.rental.payment.cardInfo.expYear=2019;
-        // $scope.rental.payment.cardInfo.cvv=834;
-        // $scope.rental.payment.cardInfo.customer=null;
-        // $scope.rental.payment.cardInfo.priority=null;
-        // $scope.rental.payment.cardInfo.name="Ravi";
-        //
-        // $scope.rental.vehicle = $scope.accordion.selectedCarFullDetails;
-        // $scope.rental.startDate = 1441218600000;
-        // $scope.rental.endDate = 1443810600000;
-        // $scope.rental.price = 100;
-        // $scope.rental.customer1 = $scope.customer;
-        // $scope.rental.customer1.dob=336767400000;
-        // $scope.rental.customer1.dlExperiryDate=1441218600000;
-        // $scope.rental.insuranceStatus="ON";
-        // $scope.rental.rentalPaymentId=2;
-        // $scope.rental.customer2 = $scope.customer2;
+        $scope.rental.payment.cardInfo=$scope.payment.cardInfo;
 
+        $scope.rental.payment.cardInfo.id=4;
+        $scope.rental.payment.cardInfo.type = "Debit";
+        $scope.rental.payment.cardInfo.number = "4356234589012341";
+        $scope.rental.payment.cardInfo.expMonth = 6;
+        $scope.rental.payment.cardInfo.expYear = 2019;
+        $scope.rental.payment.cardInfo.cvv = 834;
+        $scope.rental.payment.cardInfo.customer = null;
+        $scope.rental.payment.cardInfo.priority = null;
+        $scope.rental.payment.cardInfo.name = "Ravi";
+console.log($scope.payment);
+        $scope.rental.vehicle = $scope.accordion.selectedCarFullDetails;
+
+        $scope.rental.startDate = 1441218600000;
+        $scope.rental.endDate = 1443810600000;
+        $scope.rental.price = 100;
+
+        $scope.rental.customer1 = $scope.customer;
+        $scope.rental.customer1.dob = new Date($scope.customer.dob).getTime();
+        $scope.rental.customer1.dlExperiryDate = 1441218600000;
+        $scope.rental.customer1.dlExperiryDate = new Date($scope.customer.dlExperiryDate).getTime();
+
+        $scope.rental.insuranceStatus = "ON";
+        $scope.rental.rentalPaymentId = 2;
+        $scope.rental.customer2 = $scope.customer2;
         $http.post(ClientConfig.CLIENT_BASE_URL + "rental", $scope.rental).success(function (response) {
             $state.go("agreement");
             fleetMagicService.setRentalAgreement(response);
@@ -93,26 +174,4 @@ angular.module('fleetMagic').controller('RentalController', ['$scope', '$http', 
         })
     };
 
-    $scope.printDiv = function () {
-        var printContents = document.getElementById("rentalAgreement").innerHTML;
-        var popupWin = window.open('', '_blank', 'width=300,height=300');
-        popupWin.document.open();
-        popupWin.document.write('<html><head><link rel="stylesheet" href="css/fleetMagic.css"></head><body onload="window.print()">' + printContents + '</body></html>');
-        popupWin.document.close();
-    };
-
-    $scope.saveAsPdf = function () {
-        html2canvas(document.getElementById('rentalAgreement'), {
-            onrendered: function (canvas) {
-                var data = canvas.toDataURL();
-                var docDefinition = {
-                    content: [{
-                        image: data
-                    }]
-                };
-                pdfMake.createPdf(docDefinition).download("Score_Details.pdf");
-            },
-            logging: true
-        });
-    }
 }]);
