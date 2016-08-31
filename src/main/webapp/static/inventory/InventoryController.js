9409e32266c45b2bf868a9d17b6b12927a8dfc6d
ravia <ravia@gamenous.com>
2016-08-31 18:19:34 +0530

    Inventory tab changes

--- src/main/webapp/static/inventory/InventoryController.js
+++ src/main/webapp/static/inventory/InventoryController.js
@@ -23,9 +23,62 @@ angular.module('fleetMagic').controller('InventoryController', ['$scope', '$http
     $scope.showCarDetails = function (car) {
         for (var i = 0; i < $scope.inventory.vehicles.length; i++) {
             if (car.id == $scope.inventory.vehicles[i].id) {
-                $scope.inventory.selectedCarFullDetails = $scope.inventory.vehicles[i];
+                $scope.inventory.vehicle = $scope.inventory.vehicles[i];
             }
         }
         $scope.inventory.showDetails = true;
     };
+
+    $scope.updateVehicle = function () {
+        $http.post(ClientConfig.CLIENT_BASE_URL + "updateVehicle", convertDates($scope.inventory.vehicle)).success(function (res) {
+            console.log(res);
+        });
+    };
+
+    function convertDates(vehicle) {
+        vehicle.nextInspectionDate = new Date(vehicle.nextInspectionDate).getTime();
+        vehicle.roadTaxExpiryDate = new Date(vehicle.roadTaxExpiryDate).getTime();
+        vehicle.insuranceExpiryDate = new Date(vehicle.insuranceExpiryDate).getTime();
+        vehicle.purchaseDate = new Date(vehicle.purchaseDate).getTime();
+        vehicle.lastServiceDate = new Date(vehicle.lastServiceDate).getTime();
+        vehicle.dispenseDate = new Date(vehicle.dispenseDate).getTime();
+        console.log(vehicle);
+        return vehicle;
+    }
+
+    $scope.addVehicle = function () {
+        $http.post(ClientConfig.CLIENT_BASE_URL + "addVehicle", convertDates($scope.inventory.vehicle)).success(function (res) {
+            console.log(res);
+        })
+    };
+
+    $scope.lastServiceDate = {
+        opened: false,
+        open: open
+    };
+    $scope.dispenseDate = {
+        opened: false,
+        open: open
+    };
+
+    $scope.nextInspectionDate = {
+        opened: false,
+        open: open
+    };
+    $scope.roadTaxExpiryDate = {
+        opened: false,
+        open: open
+    };
+
+    $scope.insuranceExpiryDate = {
+        opened: false,
+        open: open
+    };
+    $scope.purchaseDate = {
+        opened: false,
+        open: open
+    };
+    function open() {
+        this.opened = true;
+    }
 }]);
