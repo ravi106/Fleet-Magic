3755d6b9c1867acfb3f921a65a4bc81de43a93d3
Ravi Kumar Auchuri <ravia@kavacloud.com>
2016-09-02 11:56:12 +0530

    updating customer fix

--- src/main/java/com/fleetmagic/cm/controller/CustomerController.java
+++ src/main/java/com/fleetmagic/cm/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class CustomerController {
 	}
 	
 	@RequestMapping(value = "/customer", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public Customer updateCustomer(Customer customer) {
+	public Customer updateCustomer(@RequestBody Customer customer) {
 
 		System.out.println("CustomerController.updateCustomer()");
 
