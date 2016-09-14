d48bb6408ff83a0fee6ebdc0bd0708074acabf38
Ravi Kumar Auchuri <ravia@kavacloud.com>
2016-09-14 15:44:06 +0530

    Rental number search Fix

--- src/main/java/com/fleetmagic/constants/FleetConstants.java
+++ src/main/java/com/fleetmagic/constants/FleetConstants.java
@@ -8,7 +8,7 @@ public class FleetConstants {
 	
 	public static final String FETCH_VEH_ON_BUSINESSUNIT_ON_STATUS = "SELECT * FROM vehicle v where businessunit_id = ? and status =?";
 	
-	public static final String FETCH_CUST_BY_ID = "SELECT * from Customer c where number= ?";
+	public static final String FETCH_CUST_BY_ID = "SELECT * from Customer c where id= ?";
 	
 	public static final String FETCH_CUST_BY_EMAIL = "SELECT * from Customer c where email = ?";
 	
@@ -20,7 +20,7 @@ public class FleetConstants {
 	
 	public static final String FETCH_RENTAL_BY_MOBILE = "SELECT * from rental r join customer c on r.customer1_id = c.id where c.mobile=? order by startDate Desc";
 	
-	public static final String FETCH_RENTAL_BY_NUMBER = "SELECT * from rental r where r.id=?";
+	public static final String FETCH_RENTAL_BY_NUMBER = "SELECT * from rental r where r.number=?";
 	
 	public static final String FETCH_RENTAL_BETWEEN_DATES= "SELECT * from rental r where (r.startDate between ? and ? )or (r.startDate <? and r.endDate <= ?) ";
 }
